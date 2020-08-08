b3eda45d5f81e21a70e474cdc85a15e838074ec2
John Uthus <63675144+UnafraidLyric75@users.noreply.github.com>
2020-08-07 18:10:38 -0700

    Issue#1 create database (#6)
    * Created Vehicle class
    
    * Created Vehicle Context
    
    * Set up skeliton for VehicleDB
    
    * setup connection string
    
    * fixed error
    
    * first migration
    
    * Updated database name to fix error

---
+++ 212TeamProject/Migrations/202008080029317_Initial.cs
@@ -0,0 +1,31 @@
+﻿namespace _212TeamProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Vehicles",
+                c => new
+                    {
+                        VehicleId = c.Int(nullable: false, identity: true),
+                        VehicleIdNumice = c.String(nullable: false),
+                        PlateNum = c.String(nullable: false),
+                        Make = c.String(nullable: false),
+                        Model = c.String(nullable: false),
+                        Year = c.Int(nullable: false),
+                        Color = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.VehicleId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Vehicles");
+        }
+    }
+}
---
+++ 212TeamProject/Vehicle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _212TeamProject
+{
+    public class Vehicle
+    {
+        /// <summary>
+        /// Id of vehicle
+        /// </summary>
+        public int VehicleId { get; set; }
+
+        /// <summary>
+        /// VIN of the car
+        /// </summary>
+        public string VehicleIdNumice { get; set; }
+
+        /// <summary>
+        /// Liecinse plate number
+        /// </summary>
+        public string PlateNum { get; set; }
+
+        /// <summary>
+        /// what company made the car BMW, Jeep, etc.
+        /// </summary>
+        public string Make { get; set; }
+
+        /// <summary>
+        /// What version of the car Compass, Mita, Model S
+        /// </summary>
+        public string Model { get; set; }
+
+        /// <summary>
+        /// Year the car is made
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Color of the car
+        /// </summary>
+        public string Color { get; set; }
+
+
+    }
+}
