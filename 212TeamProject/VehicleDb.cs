ca789e2fe9693cd574274a192011da1c1ab0f21c
John Uthus <john.a.uthus.jr1@gmail.com>
2020-08-07 17:12:34 -0700

    Set up skeliton for VehicleDB

---
+++ 212TeamProject/VehicleDb.cs
@@ -0,0 +1,41 @@
+﻿using _212TeamProject;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCRUDApp
+{
+    static class VehilceDb
+    {
+        public static List<Vehicle> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vehicle Add(Vehicle v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vehicle Update(Vehicle v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Delete(Vehicle v)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public static void Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
