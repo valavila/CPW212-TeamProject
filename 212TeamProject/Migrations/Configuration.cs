097ca9cc4540b33390f2e0c3fc914a48f78fabb9
John Uthus <john.a.uthus.jr1@gmail.com>
2020-08-09 21:25:13 -0700

    Updated any errors from previous commit

--- 212TeamProject/Migrations/Configuration.cs
+++ 212TeamProject/Migrations/Configuration.cs
@@ -5,14 +5,14 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<_212TeamProject.VehicleContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<_212TeamProject.VehicleModel>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(_212TeamProject.VehicleContext context)
+        protected override void Seed(_212TeamProject.VehicleModel context)
         {
             //  This method will be called after migrating to the latest version.
 
