d412cece510510c4c71cb351597e40c4dad8aea7
John Uthus <63675144+UnafraidLyric75@users.noreply.github.com>
2020-08-09 23:44:24 -0700

    Populate list box with corect car info (#28)

--- 212TeamProject/Form1.Designer.cs
+++ 212TeamProject/Form1.Designer.cs
@@ -28,80 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.EnterBtn = new System.Windows.Forms.Button();
             this.CarListCbox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.ShowCarsBtn = new System.Windows.Forms.Button();
             this.ChosenCarsLBox = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // EnterBtn
-            // 
-            this.EnterBtn.Location = new System.Drawing.Point(103, 162);
-            this.EnterBtn.Name = "EnterBtn";
-            this.EnterBtn.Size = new System.Drawing.Size(160, 64);
-            this.EnterBtn.TabIndex = 0;
-            this.EnterBtn.Text = "Enter";
-            this.EnterBtn.UseVisualStyleBackColor = true;
-            this.EnterBtn.Click += new System.EventHandler(this.button1_Click);
-            // 
             // CarListCbox
             // 
             this.CarListCbox.FormattingEnabled = true;
-            this.CarListCbox.Location = new System.Drawing.Point(179, 92);
+            this.CarListCbox.Location = new System.Drawing.Point(119, 60);
+            this.CarListCbox.Margin = new System.Windows.Forms.Padding(2);
             this.CarListCbox.Name = "CarListCbox";
-            this.CarListCbox.Size = new System.Drawing.Size(261, 28);
+            this.CarListCbox.Size = new System.Drawing.Size(175, 21);
             this.CarListCbox.TabIndex = 1;
             this.CarListCbox.SelectedIndexChanged += new System.EventHandler(this.CarListCbox_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 92);
+            this.label1.Location = new System.Drawing.Point(29, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.Size = new System.Drawing.Size(68, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Choose Car: ";
-            // 
-            // ShowCarsBtn
-            // 
-            this.ShowCarsBtn.Location = new System.Drawing.Point(103, 283);
-            this.ShowCarsBtn.Name = "ShowCarsBtn";
-            this.ShowCarsBtn.Size = new System.Drawing.Size(160, 72);
-            this.ShowCarsBtn.TabIndex = 3;
-            this.ShowCarsBtn.Text = "Show Chosen Car(s)";
-            this.ShowCarsBtn.UseVisualStyleBackColor = true;
             // 
             // ChosenCarsLBox
             // 
             this.ChosenCarsLBox.FormattingEnabled = true;
-            this.ChosenCarsLBox.ItemHeight = 20;
-            this.ChosenCarsLBox.Location = new System.Drawing.Point(440, 148);
+            this.ChosenCarsLBox.Location = new System.Drawing.Point(32, 134);
+            this.ChosenCarsLBox.Margin = new System.Windows.Forms.Padding(2);
             this.ChosenCarsLBox.Name = "ChosenCarsLBox";
-            this.ChosenCarsLBox.Size = new System.Drawing.Size(228, 104);
+            this.ChosenCarsLBox.Size = new System.Drawing.Size(262, 69);
             this.ChosenCarsLBox.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(446, 92);
+            this.label2.Location = new System.Drawing.Point(29, 119);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(208, 20);
+            this.label2.Size = new System.Drawing.Size(139, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Chosen Cars Specifications:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 292);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.ChosenCarsLBox);
-            this.Controls.Add(this.ShowCarsBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.CarListCbox);
-            this.Controls.Add(this.EnterBtn);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Car Picker";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -111,11 +92,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button EnterBtn;
         private System.Windows.Forms.ComboBox CarListCbox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button ShowCarsBtn;
         private System.Windows.Forms.ListBox ChosenCarsLBox;
         private System.Windows.Forms.Label label2;
     }
